1ad7685a3ca31d7e101305aff76dc5442ac7dacb
Yulia Zamriy <yulia.zamriy@gmail.com>
2018-01-26 21:40:46 -0500

    whateer

--- Working_files/W203_Lab_1_Candidate_Debt.R
+++ Working_files/W203_Lab_1_Candidate_Debt.R
@@ -1,11 +1,39 @@
-setwd("C:/Users/yzamriy/Documents/Tools and Methodology/DS/Git/Berkeley/W203-6-Lab1/Working_files/")
+#setwd("C:/Users/yzamriy/Documents/Tools and Methodology/DS/Git/Berkeley/W203-6-Lab1/Working_files/")
+#setwd("C:/Users/Zamriyka/Documents/GitHub/W203-6-Lab1/Working_files/")
 
-var_names <- colnames(read.csv("CandidateDebt.csv", nrows = 1))
+load("CandidateDebt.RData")
 
-var_names_corrected <- c(var_names[1:grep("position", var_names)],
-                         "position2",
-                         var_names[(grep("position", var_names) + 1):(length(var_names) - 1)])
+str(CandidateDebt)
 
-CandidateDebt <- read.csv("CandidateDebt.csv", 
-                          stringsAsFactors = FALSE,
-                          col.names = var_names_corrected)
+summary(CandidateDebt$amount)
+
+length(unique(CandidateDebt$reportnumber))
+# 987
+length(unique(CandidateDebt$filerid))
+# 140
+length(unique(CandidateDebt$filername))
+# 133
+
+aggr_filer <- aggregate(amount ~ filername + filerid, data = CandidateDebt, sum)
+aggr_filer2 <- aggregate(filerid ~ filername, data = aggr_filer, length)
+dups <- aggr_filer2$filername[aggr_filer2$filerid > 1]
+CandidateDebt[CandidateDebt$filername == dups[3],]
+
+aggr_office <- aggregate(amount ~ filerid + office, data = CandidateDebt, sum)
+aggr_office2 <- aggregate(office ~ filerid, data = aggr_office, length)
+table(aggr_office2$office)
+#   1 
+# 140 
+
+aggr_party <- aggregate(amount ~ filerid + party, data = CandidateDebt, sum)
+aggr_party2 <- aggregate(party ~ filerid, data = aggr_party, length)
+table(aggr_party2$party)
+# 1  2  3 
+#50 63 27 
+
+library(tidyverse)
+
+aggr_cand <- CandidateDebt %>% 
+  group_by(filerid) %>% 
+  summarize(ttl_amount = sum(amount),
+            office = first(office))
